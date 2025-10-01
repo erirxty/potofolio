28a77e7bb15ee2821304ef1966b635c1d3341659
土屋絵里佳 <>
2025-10-01 22:37:06 +0900

    ディレクトリー変更

--- portfolio1/functions.php
+++ portfolio1/functions.php
@@ -2,8 +2,8 @@
 // CSSとJavaScriptを登録・読み込みする関数
 function portfolio_enqueue_scripts() {
     // CSSとJSを読み込みます
-    wp_enqueue_style( 'main-style', get_template_directory_uri() . '/style.css', array(), '1.0.0' );
-    wp_enqueue_script( 'main-script', get_template_directory_uri() . '/main.js', array(), '1.0.0', true );
+    wp_enqueue_style( 'main-style', get_template_directory_uri() . '/style/style.css', array(), '1.0.0' );
+    wp_enqueue_script( 'main-script', get_template_directory_uri() . '/js/main.js', array(), '1.0.0', true );
 }
 // WordPressに上記の関数を実行するよう伝えます
 add_action( 'wp_enqueue_scripts', 'portfolio_enqueue_scripts' );
